cf4823d485c6e2bab5e6044e385e1f52b5b215fa
Roger Infa <rinfas@ulasalle.edu.pe>
2025-06-01 23:34:43 -0500

    Modelo user base creado

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,16 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm"
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({unique: true})
+    username: string;
+
+    @Column()
+    password: string;
+
+    @Column({ type: 'datetime', default: () => 'CURRENT_TIMESTAMP' })
+    createdAt: Date;
+}
