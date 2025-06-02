b380ea2e5f4ff84b4da80777dfb5f5c9d5a08123
Roger Infa <rinfas@ulasalle.edu.pe>
2025-06-02 13:06:56 -0500

    Ruta /profile protegida

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
+import { AuthGuard } from './guard/auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -21,6 +22,13 @@ export class AuthController {
         loginDto: LoginDto,
     ) {
         return this.authService.login(loginDto);
+    }
+
+    //Ruta protegida con guard
+    @Get('profile')
+    @UseGuards(AuthGuard)
+    profile() {
+        return 'profile';
     }
     
 }
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
+import { AuthGuard } from 'src/auth/guard/auth.guard';
 
 @Controller('tasks')
 export class TasksController {
@@ -13,6 +14,7 @@ export class TasksController {
   }
 
   @Get()
+  @UseGuards(AuthGuard)
   findAll() {
     return this.tasksService.findAll();
   }
