243822b8e5269a7f6689ec63dabed675bfe81e0d
Roger Infa <rinfas@ulasalle.edu.pe>
2025-06-01 20:39:26 -0500

    Agrefo typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '147895326',
+      database: 'test_01',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, //shouldn't be used in production 
+      }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
