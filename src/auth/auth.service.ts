e53e9b7c1d38e20ffba11985911b5f6952267241
Roger Infa <rinfas@ulasalle.edu.pe>
2025-06-02 01:18:28 -0500

    Creacion de token completa

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,10 +3,14 @@ import { UsersService } from 'src/users/users.service';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
 import * as bcrypt from 'bcrypt'; 
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-    constructor(private readonly usersService: UsersService){}
+    constructor(
+        private readonly usersService: UsersService,
+        private readonly jwtService: JwtService,
+    ){}
 
     async register(registerDto: RegisterDto) {
         return await this.usersService.create(registerDto);
@@ -24,6 +28,10 @@ export class AuthService {
             throw new UnauthorizedException('Invalid password');
         }
         // Ahora token JWT
-        return user;
+        const payload = { username: user.username};
+        const token = await this.jwtService.signAsync(payload);
+        return {
+            token,
+        };
     }
 }
